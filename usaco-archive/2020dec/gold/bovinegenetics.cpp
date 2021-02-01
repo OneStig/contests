9343c49c1c587b3fcde200ae7306b623c7a13b6a
Steven He <therealonestig@gmail.com>
2021-01-31 17:25:48 -0800

    Fix

--- usaco-archive/2020dec/gold/bovinegenetics.cpp
+++ usaco-archive/2020dec/gold/bovinegenetics.cpp
@@ -10,42 +10,54 @@ int main()
 
     int MOD = 1000000007;
 
-    int dp[100001][4][4][4];
-    char BASES[4] = {'A', 'G', 'C', 'T'};
+    char bfor[4] = {'A', 'G', 'C', 'T'};
+    map<char, int> brev = {
+        {'A', 0},
+        {'G', 1},
+        {'C', 2},
+        {'T', 3}
+    };
 
-    for (int n = 0; n < 4; n++)
+    int dp[100001][4][4][4];
+
+    memset(dp, 0, sizeof(dp));
+
+    for (int i = 0; i < 4; i++)
     {
-        for (int l = 0; l < 4; l++)
-        {
-            if (s[0] == '?' || s[0] == BASES[l])
-            {
-                dp[0][n][l][l] = 1;
+        if (s[i] == '?') {
+            for (int j = 0; j < 4; j++) {
+                dp[0][i][j][j] = 1;
             }
+        }
+        else {
+            dp[0][i][brev[s[i]]][brev[s[i]]] = 1;
         }
     }
 
-    for (int k = 1; k < s.length(); k++)
+    for (int i = 1; i < s.length(); i++)
     {
-        for (int m2 = 0; m2 < 4; m2++)
+        for (int j = 0; j < 4; j++)
         {
-            if (s[k] == '?' || s[k] == BASES[m2])
+            if (s[i] == '?' || s[i] == bfor[j])
             {
-                for (int n = 0; n < 4; n++)
+                for (int k = 0; k < 4; k++)
                 {
                     for (int l = 0; l < 4; l++)
                     {
                         for (int m = 0; m < 4; m++)
                         {
-                            if (m != m2)
+                            if (m != j)
                             {
-                                dp[k][n][l][m2] += dp[k - 1][n][l][m];
-                                dp[k][n][l][m2] %= MOD;
+                                dp[i][k][l][j] += dp[i - 1][k][l][m];
                             }
-                            if (n == m)
+                            if (k == m)
                             {
-                                dp[k][l][m2][m2] += dp[k - 1][n][l][m];
-                                dp[k][l][m2][m2] %= MOD;
+                                dp[i][l][j][j] += dp[i - 1][k][l][m];
+                                
                             }
+
+                            dp[i][k][l][j] %= MOD;
+                            dp[i][l][j][j] %= MOD;
                         }
                     }
                 }
@@ -53,16 +65,16 @@ int main()
         }
     }
 
-    long answer = 0;
-    for (int l = 0; l < 4; l++)
+    long long sol = 0;
+
+    for (int i = 0; i < 4; i++)
     {
-        for (int m = 0; m < 4; m++)
+        for (int j = 0; j < 4; j++)
         {
-            answer += dp[s.length() - 1][m][l][m];
-            answer %= MOD;
+            sol += dp[s.length() - 1][j][i][j];
+            sol %= MOD;
         }
     }
-    answer %= MOD;
 
-    cout << answer << endl;
+    cout << sol << endl;
 }
